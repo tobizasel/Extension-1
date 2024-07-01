3c3b8630364e3d2440c2347283412e250f2e266f
TOBIAS ZASELSKY <47435897@ort>
2024-07-01 12:01:36 -0300

    ahre

--- script.js
+++ script.js
@@ -1,65 +1,61 @@
-const url = "https://i.ibb.co/Mht5qh2/logo.png";
-let index = 5;
-let tweetsArray = [];
+    const url = "https://i.ibb.co/Mht5qh2/logo.png";
+    const logo_normal = "https://i.ibb.co/yq25QXG/logo.png"
+    const logo_borde = "https://i.ibb.co/tL8b3qZ/logo-con-borde.png"
+    const logo_sinLupa = "https://i.ibb.co/rHcZPqZ/logo-lupa.png"
+    let index = 5;
+    let tweetsArray = [];
 
-function cambiarColor() {
-  document.getElementsByTagName("body")[0].style.backgroundColor = "red";
-  const h2 = document.createElement("h2");
-  h2.textContent = "HOLAAAAA";
-  document.getElementsByTagName("body")[0].appendChild(h2);
-}
-
-function tweet(tweets) {
-  tweets.forEach((twit) => {
-    if (twit.getAttribute('estado') === "true") {
-      twit.classList.add("verdadero");
-    } else if (twit.getAttribute('estado') === "false") {
-      twit.classList.add("falso");
-    }
-
-    // Check if logo is already appended
-    if (!twit.querySelector(".logo")) {
-      const logo = document.createElement("img");
-      logo.setAttribute("src", url);
-      logo.setAttribute("class", "logo");
-      twit.appendChild(logo); // Agregar el logo al artículo del tweet
-    }
-  });
-}
-
-async function waitForTweets() {
-  const tweets = document.getElementsByTagName("article");
-  const newTweetsArray = Array.from(tweets);
-  
-  const uniqueTweetsSet = new Set([...tweetsArray, ...newTweetsArray]);
-  
-  tweetsArray = Array.from(uniqueTweetsSet);
-  console.log(index, "index");
-
-  console.log(".length:" + tweetsArray.length, "/ index:" + index);
-
-  if (tweetsArray.length <= index) {
-    tweetsArray.forEach((twit) => {
-
-      if (twit.getAttribute('estado') === null ) { // Only assign estado if it hasn't been set yet
-        const real = Math.floor(Math.random() * 2) + 1;
-        if (real === 1) {
-          twit.setAttribute("estado", "true");
-        } else if (real === 2) {
-          twit.setAttribute("estado", "false");
+    function tweet(tweets) {
+      tweets.forEach((twit) => {
+        if (twit.getAttribute('estado') === "true") {
+          twit.classList.add("verdadero");
+        } else if (twit.getAttribute('estado') === "false") {
+          twit.classList.add("falso");
         }
-      }
-      agarrarTexto(twit)
-    });
-    tweet(tweetsArray); // Call tweet with the correct array
-    index += 5;
-  }
-}
 
-async function agarrarTexto(twit){
-  console.log(twit.children[0].children[0].children[1].children[1].children[1].children[0].children[0].textContent);
-}
+        // Check if logo is already appended
+        if (!twit.querySelector(".logo")) {
+          const logo = document.createElement("img");
+          logo.setAttribute("src", logo_sinLupa);
+          logo.setAttribute("class", "logo");
+          twit.appendChild(logo); // Agregar el logo al artículo del tweet
+        }
+      });
+    }
+
+    async function waitForTweets() {
+      const tweets = document.getElementsByTagName("article");
+      const newTweetsArray = Array.from(tweets);
+      
+      const uniqueTweetsSet = new Set([...tweetsArray, ...newTweetsArray]);
+      
+      tweetsArray = Array.from(uniqueTweetsSet);
+      console.log(tweetsArray);
+      console.log(index, "index");
+
+      console.log(".length:" + tweetsArray.length, "/ index:" + index);
+
+      
+        tweetsArray.forEach((twit) => {
+          console.log(twit.getAttribute('estado'));
+          if (twit.getAttribute('estado') === null ) { // Only assign estado if it hasn't been set yet
+            const real = Math.floor(Math.random() * 2) + 1;
+            if (real === 1) {
+              twit.setAttribute("estado", "true");
+            } else if (real === 2) {
+              twit.setAttribute("estado", "false");
+            }
+          }
+          agarrarTexto(twit)
+        });
+        tweet(tweetsArray); // Call tweet with the correct array
+        index += 5;
+    }
+
+    async function agarrarTexto(twit){
+      console.log(twit.children[0].children[0].children[1].children[1].children[1].children[0].children[0].textContent);
+    }
 
 
-const interval = setInterval(waitForTweets, 5000); // Verificar cada 3 segundos
+    const interval = setInterval(waitForTweets, 1000); // Verificar cada 3 segundos
 
